ded8f698f38b2f777a7cb4ac44deab9297ea7035
agharibipour <amir.gharibipour@ubisoft.com>
2026-06-04 14:29:50 -0400

    finished

--- go.mod
+++ go.mod
@@ -5,7 +5,13 @@ go 1.25.4
 require (
 	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203 // indirect
 	github.com/grandcat/zeroconf v1.0.0 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.9.9 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
 	github.com/klauspost/reedsolomon v1.12.0 // indirect
 	github.com/miekg/dns v1.1.66 // indirect
@@ -15,10 +21,10 @@ require (
 	github.com/tjfoc/gmsm v1.4.1 // indirect
 	github.com/xtaci/kcp-go/v5 v5.6.70 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
-	golang.org/x/mod v0.25.0 // indirect
+	golang.org/x/mod v0.28.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sync v0.15.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/time v0.14.0 // indirect
-	golang.org/x/tools v0.34.0 // indirect
+	golang.org/x/tools v0.37.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,14 +2,72 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
-	"time"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/poisnoir/spine-go"
 )
 
+var ns *spine.Namespace
+var pub *spine.Publisher[[4][4]float64]
+
+type Game struct{}
+
+// Update runs 60 times per second. Put your input logic here.
+func (g *Game) Update() error {
+	var goal [4][4]float64
+
+	goal[0][0] = 1
+	goal[1][1] = 1
+	goal[2][2] = 1
+	goal[3][3] = 1
+
+	// 1. Check for single key presses (e.g., pressing Escape to quit)
+	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
+		return ebiten.Termination // Quits the program safely
+	}
+
+	// 2. Check for SIMULTANEOUS key holds
+	if ebiten.IsKeyPressed(ebiten.KeyW) {
+		goal[1][3] += 0.001
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyS) {
+		goal[1][3] -= 0.001
+		fmt.Print("Moving DOWN ↓ ")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		goal[2][3] += 0.001
+		fmt.Print("Moving LEFT ← ")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyD) {
+		goal[2][3] -= 0.001
+		fmt.Print("Moving RIGHT → ")
+	}
+
+	// If any keys were pressed, print a new line so it's readable
+	if ebiten.IsKeyPressed(ebiten.KeyW) || ebiten.IsKeyPressed(ebiten.KeyS) ||
+		ebiten.IsKeyPressed(ebiten.KeyA) || ebiten.IsKeyPressed(ebiten.KeyD) {
+		fmt.Println()
+	}
+
+	pub.Publish(goal)
+
+	return nil
+}
+
+// Draw is required by Ebiten, but we can leave it empty since we are using the console
+func (g *Game) Draw(screen *ebiten.Image) {}
+
+// Layout defines the window size
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
 func main() {
+
 	namespace := flag.String("namespace", "rime", "spine namespace to join")
 	name := flag.String("name", "r1-change", "publisher name")
 	key := flag.String("key", "ppap", "spine namespace key")
@@ -18,32 +76,23 @@ func main() {
 
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	ns, err := spine.JointNamespace(*namespace, *key, logger)
+	var err error
+
+	ns, err = spine.JointNamespace(*namespace, *key, logger)
 	if err != nil {
 		panic(err)
 	}
 
-	pub, err := spine.NewPublisher[[4][4]float64](ns, *name)
+	pub, err = spine.NewPublisher[[4][4]float64](ns, *name)
 	if err != nil {
 		panic(err)
 	}
 
-	var goal [4][4]float64
-	for {
+	ebiten.SetWindowTitle("Keyboard Tracker")
+	ebiten.SetWindowSize(320, 240)
 
-		// don't change orientation
-		goal[0][0] = 1
-		goal[1][1] = 1
-		goal[2][2] = 1
-		goal[3][3] = 1
-
-		// just z for now
-		goal[1][3] = 0.0001
-
-		pub.Publish(goal)
-
-		// make it slow :(
-		time.Sleep(50 * time.Microsecond)
+	// Run the game loop
+	if err := ebiten.RunGame(&Game{}); err != nil && err != ebiten.Termination {
+		panic(err)
 	}
-
 }
